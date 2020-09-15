3b5cd5edc93a8b431cff6d0fd356842c1a67a246
nkari <nkari82@gmail.com>
2020-09-15 22:10:31 +0900

    v0.1 (japanese)

--- src/example.cpp
+++ src/example.cpp
@@ -9,8 +9,7 @@ int main()
 
 	IProcessor::Ptr ptr = IProcessor::Create("-rD:/Github/unidic-mecab-2.1.2/mecabrc");
 	std::vector<int32_t> seq;
-	ptr->ToSeq("また東寺のように五大明王と呼ばれる主要な明王の中央に配されることも多い。", seq);
-	//ptr->ToSeq("안녕하세요", seq);
+	ptr->ToSeq("また東寺のように五大明王と呼ばれる主要な明王の中央に配されることも多い。", seq, "CP949");
 		
 	return 0;
 }
--- src/ftts.cpp
+++ src/ftts.cpp
@@ -4,66 +4,46 @@
 #define MECAB_STATIC
 #include <unordered_map>
 #include <cassert>
-#include <uchardet.h>
 #include <iconv.h>
 #include <utf8proc.h>
 #include <mecab.h>
+#include <string>
 #include <cstring>
 #include <list>
 #include <algorithm>
 #include <iostream>
 #include <cctype>
+#include <sstream>
 
 namespace ftts
 {
 	// https://mothereff.in/utf-8
 	// https://onlineutf8tools.com/convert-utf32-to-utf8
 	// https://unicodelookup.com/
-	using UString = std::basic_string<int32_t>;
-
-	static utf8proc_option_t NFKC = utf8proc_option_t(0
-			|UTF8PROC_NULLTERM
-			|UTF8PROC_STABLE
-			|UTF8PROC_COMPOSE
-			|UTF8PROC_COMPAT
-			|UTF8PROC_CASEFOLD
-			|UTF8PROC_IGNORE);
 
 	class Processor : public IProcessor
 	{
-	protected:
-		using Symbols = std::unordered_map<size_t, int32_t>;
-		Symbols		symbols_;
-
 	private:
 		using Encodings = std::unordered_map<size_t, iconv_t>;
-		Encodings	encodings_;
-		uchardet*		ud_;
+		Encodings encodings_;
 
 	public:
 		Processor()
-		{
-			ud_ = uchardet_new();
-		}
+		{}
 
 		virtual ~Processor()
 		{
-			uchardet_delete(ud_);
 			for (auto& pair : encodings_)
 				iconv_close(pair.second);
 			encodings_.clear();
 		}
 
-		void ToUnicode(UString& out, const char* text, const char* enc = nullptr)
+		void ToUTF8(std::string& out, const char* text, const char* enc = nullptr)
 		{
 			size_t length = std::strlen(text);
 
-			if( enc == nullptr)
-				enc = Detect(text, length);
-
 			size_t bufsize(256);
-			std::string str;
-			str.resize(bufsize);
+			out.resize(bufsize);
 
 			const char* utf8 = "UTF-8";
 			bool is_utf8 = std::equal(enc, enc+std::strlen(enc), utf8, utf8+std::strlen(utf8), [](char a, char b)
@@ -73,7 +53,7 @@ namespace ftts
 
 			if (std::strlen(enc) == 0 || is_utf8)
 			{
-				str = text;
+				out = text;
 			}
 			else
 			{
@@ -96,7 +76,7 @@ namespace ftts
 				size_t pos = 0;
 				while (0 < insize)
 				{
-					char* outptr = &str[pos];
+					char* outptr = &out[pos];
 					size_t outsize = bufsize;
 
 					size_t res = ::iconv(cd, (const char**)&inptr, &insize, &outptr, &outsize);
@@ -114,55 +94,36 @@ namespace ftts
 					size_t length = bufsize - outsize;
 					pos += length;
 					bufsize -= (length - insize);
-					str.append(outptr);
-					str.resize(pos + insize);			// fit
+					out.append(outptr);
+					out.resize(pos + insize);			// fit
 					assert(insize == 0);
 				}
 			}
-
-			utf8proc_int32_t cp = 0;
-			const utf8proc_uint8_t* pstring = reinterpret_cast<const utf8proc_uint8_t*>(&str.front());
-			utf8proc_ssize_t size = str.size();
-			utf8proc_ssize_t n(0);
-			while ((n = utf8proc_iterate(pstring, size, &cp)) > 0)
-			{
-				out.push_back(cp);
-				pstring += n;
-				size -= n;
-			}
 		}
 		
-		void Replace(UString& message, UString& pattern, UString& replace) 
+		void Replace(std::string& message, std::string& pattern, std::string& replace) 
 		{
-			UString::size_type pos = 0;
-			UString::size_type offset = 0;
-			while ((pos = message.find(pattern, offset)) != UString::npos)
+			std::string::size_type pos = 0;
+			std::string::size_type offset = 0;
+			while ((pos = message.find(pattern, offset)) != std::string::npos)
 			{
 				message.replace(message.begin() + pos, message.begin() + pos + pattern.size(), replace);
 				offset = pos + replace.size();
 			}
 		}
-	
-	private:
-		const char* Detect(const char* text, size_t length)
-		{
-			uchardet_reset(ud_);
-			int ret = uchardet_handle_data(ud_, text, length);
-			uchardet_data_end(ud_);
-			return (char*)uchardet_get_charset(ud_);
-		}
 	};
 
-#define b1(a) (char)a
-#define b2(a, b) (char)a, (char)b
-#define b3(a, b, c) (char)a, (char)b, (char)c
+#define b1(a) {(char)a}
+#define b2(a, b) {(char)a, (char)b}
+#define b3(a, b, c) {(char)a, (char)b, (char)c}
 	class JSProcessor : public Processor
 	{
 	private:
+		using Symbols = std::unordered_map<int32_t, int32_t>;
 		int32_t eos_;
-
+		Symbols symbols_;
 		MeCab::Tagger* tagger_;
-
+		std::string empty_;
 		std::vector<std::string> punctuation_;
 		std::vector<std::string> cleaner_;
 		std::vector<std::pair<std::string, std::string>> normalize_;	
@@ -171,62 +132,63 @@ namespace ftts
 		JSProcessor(const char* args)
 			: eos_(0xffffffff)
 			, tagger_(MeCab::createTagger(args))
+			, empty_("")
 			, punctuation_
 			{ 
-				{b1(0x2e)}, // "."
-				{b1(0x2c)}, // ","
-				{b3(0xe3,0x80,0x81)}, // "、"
-				{b3(0xe3,0x80,0x82)}, // "。"
-				{b3(0xef,0xbc,0x81)}, // "！"
-				{b3(0xef,0xbc,0x9f)}, // "？"
-				{b1(0x21)}, // "!"
-				{b1(0x3f)} // "?"
+				b1(0x2e), // "."
+				b1(0x2c), // ","
+				b3(0xe3,0x80,0x81), // "、"
+				b3(0xe3,0x80,0x82), // "。"
+				b3(0xef,0xbc,0x81), // "！"
+				b3(0xef,0xbc,0x9f), // "？"
+				b1(0x21), // "!"
+				b1(0x3f) // "?"
 			}	
 			, cleaner_	
 			{ 
-				{b1(0x20)},  // " "
-				{b3(0xe3,0x80,0x80)}, // "　"
-				{b3(0xe3,0x80,0x8c)}, // "「"
-				{b3(0xe3,0x80,0x8d)}, // "」"
-				{b3(0xe3,0x80,0x8e)}, // "『"
-				{b3(0xe3,0x80,0x8f)}, // "』"
-				{b1(0x3f)}, // "・"
-				{b3(0xe3,0x80,0x90)}, // "【"
-				{b3(0xe3,0x80,0x91)}, // "】"
-				{b3(0xef,0xbc,0x88)}, // "（"
-				{b3(0xef,0xbc,0x89)}, // "）"
-				{b1(0x28)}, // "("
-				{b1(0x29)} // ")"
+				b1(0x20),  // " "
+				b3(0xe3,0x80,0x80), // "　"
+				b3(0xe3,0x80,0x8c), // "「"
+				b3(0xe3,0x80,0x8d), // "」"
+				b3(0xe3,0x80,0x8e), // "『"
+				b3(0xe3,0x80,0x8f), // "』"
+				b1(0x3f), // "・"
+				b3(0xe3,0x80,0x90), // "【"
+				b3(0xe3,0x80,0x91), // "】"
+				b3(0xef,0xbc,0x88), // "（"
+				b3(0xef,0xbc,0x89), // "）"
+				b1(0x28), // "("
+				b1(0x29) // ")"
 			}
 			, normalize_
 			{
-				{{b3(0xe3,0x80,0x9c)}, {b3(0xe3,0x83,0xbc)}}, // replace('〜', 'ー')
-				{{b3(0xef,0xbd,0x9e)}, {b3(0xe3,0x83,0xbc)}}, // replace('～', 'ー')
-				{{b3(0xe2,0x80,0x99)}, {b1(0x27)}}, // replace("’", "'")
-				{{b3(0xe2,0x80,0x9d)}, {b1(0x22)}}, // replace('”', '"')
-				{{b3(0xe2,0x80,0x9c)}, {b2(0x60,0x60)}}, // replace('“', '``')
-				{{b2(0xcb,0x97)}, {b1(0x2d)}}, // replace('˗', '-')
-				{{b2(0xd6,0x8a)}, {b1(0x2d)}}, // replace('֊', '-')
-				{{b3(0xe2,0x80,0x90)}, {b1(0x2d)}}, // replace('‐', '-')
-				{{b3(0xe2,0x80,0x91)}, {b1(0x2d)}}, // replace('‑', '-')
-				{{b3(0xe2,0x80,0x92)}, {b1(0x2d)}}, // replace('‒', '-')
-				{{b3(0xe2,0x80,0x93)}, {b1(0x2d)}}, // replace('–', '-')
-				{{b3(0xe2,0x81,0x83)}, {b1(0x2d)}}, // replace('⁃', '-')
-				{{b3(0xe2,0x81,0xbb)}, {b1(0x2d)}}, // replace('⁻', '-')
-				{{b3(0xe2,0x82,0x8b)}, {b1(0x2d)}}, // replace('₋', '-')
-				{{b3(0xe2,0x88,0x92)}, {b1(0x2d)}}, // replace('−', '-')
-				{{b3(0xef,0xb9,0xa3)}, {b3(0xe3,0x83,0xbc)}}, // replace('﹣', 'ー')
-				{{b3(0xef,0xbc,0x8d)}, {b3(0xe3,0x83,0xbc)}}, // replace('－', 'ー')
-				{{b3(0xe2,0x80,0x94)}, {b3(0xe3,0x83,0xbc)}}, // replace('—', 'ー')
-				{{b3(0xe2,0x80,0x95)}, {b3(0xe3,0x83,0xbc)}}, // replace('―', 'ー')
-				{{b3(0xe2,0x94,0x81)}, {b3(0xe3,0x83,0xbc)}}, // replace('━', 'ー')
-				{{b3(0xe2,0x94,0x80)}, {b3(0xe3,0x83,0xbc)}}, // replace('─', 'ー')
-				{{b1(0x2c)}, {b3(0xe3,0x80,0x81)}}, // replace(',', '、')
-				{{b1(0x2e)}, {b3(0xe3,0x80,0x82)}}, // replace('.', '。')
-				{{b3(0xef,0xbc,0x8c)}, {b3(0xe3,0x80,0x81)}}, // replace('，', '、')
-				{{b3(0xef,0xbc,0x8e)}, {b3(0xe3,0x80,0x82)}}, // replace('．', '。')
-				{{b1(0x21)}, {b3(0xef,0xbc,0x81)}}, // replace('!', '！')
-				{{b1(0x3f)}, {b3(0xef,0xbc,0x81)}}	// replace('?', '？')
+				{b3(0xe3,0x80,0x9c), b3(0xe3,0x83,0xbc)}, // replace('〜', 'ー')
+				{b3(0xef,0xbd,0x9e), b3(0xe3,0x83,0xbc)}, // replace('～', 'ー')
+				{b3(0xe2,0x80,0x99), b1(0x27)}, // replace("’", "'")
+				{b3(0xe2,0x80,0x9d), b1(0x22)}, // replace('”', '"')
+				{b3(0xe2,0x80,0x9c), b2(0x60,0x60)}, // replace('“', '``')
+				{b2(0xcb,0x97), b1(0x2d)}, // replace('˗', '-')
+				{b2(0xd6,0x8a), b1(0x2d)}, // replace('֊', '-')
+				{b3(0xe2,0x80,0x90), b1(0x2d)}, // replace('‐', '-')
+				{b3(0xe2,0x80,0x91), b1(0x2d)}, // replace('‑', '-')
+				{b3(0xe2,0x80,0x92), b1(0x2d)}, // replace('‒', '-')
+				{b3(0xe2,0x80,0x93), b1(0x2d)}, // replace('–', '-')
+				{b3(0xe2,0x81,0x83), b1(0x2d)}, // replace('⁃', '-')
+				{b3(0xe2,0x81,0xbb), b1(0x2d)}, // replace('⁻', '-')
+				{b3(0xe2,0x82,0x8b), b1(0x2d)}, // replace('₋', '-')
+				{b3(0xe2,0x88,0x92), b1(0x2d)}, // replace('−', '-')
+				{b3(0xef,0xb9,0xa3), b3(0xe3,0x83,0xbc)}, // replace('﹣', 'ー')
+				{b3(0xef,0xbc,0x8d), b3(0xe3,0x83,0xbc)}, // replace('－', 'ー')
+				{b3(0xe2,0x80,0x94), b3(0xe3,0x83,0xbc)}, // replace('—', 'ー')
+				{b3(0xe2,0x80,0x95), b3(0xe3,0x83,0xbc)}, // replace('―', 'ー')
+				{b3(0xe2,0x94,0x81), b3(0xe3,0x83,0xbc)}, // replace('━', 'ー')
+				{b3(0xe2,0x94,0x80), b3(0xe3,0x83,0xbc)}, // replace('─', 'ー')
+				{b1(0x2c), b3(0xe3,0x80,0x81)}, // replace(',', '、')
+				{b1(0x2e), b3(0xe3,0x80,0x82)}, // replace('.', '。')
+				{b3(0xef,0xbc,0x8c), b3(0xe3,0x80,0x81)}, // replace('，', '、')
+				{b3(0xef,0xbc,0x8e), b3(0xe3,0x80,0x82)}, // replace('．', '。')
+				{b1(0x21), b3(0xef,0xbc,0x81)}, // replace('!', '！')
+				{b1(0x3f), b3(0xef,0xbc,0x81)}	// replace('?', '？')
 			}
 		{
 			// generate symbols (UTF32)
@@ -267,21 +229,83 @@ namespace ftts
 
 		void ToSeq(const char* text, std::vector<int32_t>& seq, const char* enc) override
 		{
-			UString unicode;
-			ToUnicode(unicode, text, enc);
-			// ToUTF8
-			// ToUTF32
+			std::string utf8;
+			ToUTF8(utf8, text, enc);
 			
 			// cleaner
-			//for (auto& c : cleaner_)
-			//	std::remove(unicode.begin(), unicode.end(), c);
-			//
-			//// normalize
-			//for (auto& pair : normalize_)
-			//	Replace(unicode, pair.first, pair.second);
+			for (auto& c : cleaner_)
+				Replace(utf8, c, empty_);
+			
+			// normalize
+			for (auto& pair : normalize_)
+				Replace(utf8, pair.first, pair.second);
 
-			//size_t length = utf8proc_normalize_utf32((utf8proc_int32_t*)unicode.c_str(), unicode.length(), NFKC);
-			//unicode.resize(length);
+			utf8proc_uint8_t* buffer = utf8proc_NFKC((const utf8proc_uint8_t*)utf8.c_str());
+			utf8.assign((const char*)buffer);
+			free(buffer);
+
+			// pronunciation
+			const char* result = tagger_->parse(utf8.c_str(), utf8.size());
+			if (result)
+			{
+				utf8.clear();
+
+				std::istringstream input(result);
+				std::string line;
+
+				while (std::getline(input, line))
+				{
+					std::istringstream row(line);
+					std::string column[2];
+
+					size_t count;
+					for (count = 0; count < 2 && std::getline(row, column[count], '\t'); ++count) {}
+					//std::cout << line << std::endl;
+
+					if (count == 2)
+					{
+						// keep punctuation
+						if (1)
+						{
+							bool found(false);
+							for (auto& p : punctuation_)
+							{
+								if (!column[0].compare(p.c_str()))
+								{
+									found = true;
+									break;
+								}
+							}
+							utf8.append(found ? column[0] : column[1]);
+						}
+						else
+						{
+							utf8.append(column[1].empty() ? column[0] : column[1]);
+						}
+					}
+				}
+			}
+
+			// for test
+			std::cout << utf8 << std::endl;
+
+			seq.clear();
+
+			const utf8proc_uint8_t* pstring = reinterpret_cast<const utf8proc_uint8_t*>(utf8.c_str());
+			utf8proc_ssize_t size = utf8.size();
+			utf8proc_int32_t cp = 0;
+			for (utf8proc_ssize_t n = 0; (n = utf8proc_iterate(pstring, size, &cp)) > 0; pstring += n, size -= n)
+			{
+				auto it = symbols_.find(cp);
+				if (it == symbols_.end())
+				{
+					std::cerr << "That code is not in the symbol => " << cp << std::endl;
+					continue;
+				}
+				seq.emplace_back(it->second);
+			}
+
+			seq.emplace_back(eos_);
 		}
 	};
 
