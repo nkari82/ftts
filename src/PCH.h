44f035667c74942339a560ec26a8f6925fcccf39
GD <nkari82@gmail.com>
2020-10-21 17:03:20 +0900

    Clean

--- src/PCH.h
+++ src/PCH.h
@@ -21,39 +21,6 @@
 #include <array>
 #include <regex>
 
-#define b1(a) {(char)a}
-#define b2(a, b) {(char)a, (char)b}
-#define b3(a, b, c) {(char)a, (char)b, (char)c}
-#define b4(a, b, c, d) {(char)a, (char)b, (char)c, (char)d}
-#define b5(a, b, c, d, e) {(char)a, (char)b, (char)c, (char)d, (char)e}
-#define b6(a, b, c, d, e, f) {(char)a, (char)b, (char)c, (char)d, (char)e, (char)f}
-#define b7(a, b, c, d, e, f, g) {(char)a, (char)b, (char)c, (char)d, (char)e, (char)f, (char)g}
-#define b8(a, b, c, d, e, f, g, h) {(char)a, (char)b, (char)c, (char)d, (char)e, (char)f, (char)g, (char)h}
-#define b9(a, b, c, d, e, f, g, h, i) {(char)a, (char)b, (char)c, (char)d, (char)e, (char)f, (char)g, (char)h, (char)i}
-#define b10(a, b, c, d, e, f, g, h, i, j) {(char)a, (char)b, (char)c, (char)d, (char)e, (char)f, (char)g, (char)h, (char)i, (char)j}
-#define b11(a, b, c, d, e, f, g, h, i, j, k) {(char)a, (char)b, (char)c, (char)d, (char)e, (char)f, (char)g, (char)h, (char)i, (char)j, (char)k}
-#define b12(a, b, c, d, e, f, g, h, i, j, k, l) {(char)a, (char)b, (char)c, (char)d, (char)e, (char)f, (char)g, (char)h, (char)i, (char)j, (char)k, (char)l}
-#define b13(a, b, c, d, e, f, g, h, i, j, k, l, m) {(char)a, (char)b, (char)c, (char)d, (char)e, (char)f, (char)g, (char)h, (char)i, (char)j, (char)k, (char)l, (char)m}
-#define b14(a, b, c, d, e, f, g, h, i, j, k, l, m, n) {(char)a, (char)b, (char)c, (char)d, (char)e, (char)f, (char)g, (char)h, (char)i, (char)j, (char)k, (char)l, (char)m, (char)n}
-#define b15(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o) {(char)a, (char)b, (char)c, (char)d, (char)e, (char)f, (char)g, (char)h, (char)i, (char)j, (char)k, (char)l, (char)m, (char)n, (char)o}
-#define b16(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p) {(char)a, (char)b, (char)c, (char)d, (char)e, (char)f, (char)g, (char)h, (char)i, (char)j, (char)k, (char)l, (char)m, (char)n, (char)o, (char)p}
-#define b17(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q) {(char)a, (char)b, (char)c, (char)d, (char)e, (char)f, (char)g, (char)h, (char)i, (char)j, (char)k, (char)l, (char)m, (char)n, (char)o, (char)p, (char)q}
-#define b18(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, s) {(char)a, (char)b, (char)c, (char)d, (char)e, (char)f, (char)g, (char)h, (char)i, (char)j, (char)k, (char)l, (char)m, (char)n, (char)o, (char)p, (char)q, (char)s}
-#define b19(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, s, t) {(char)a, (char)b, (char)c, (char)d, (char)e, (char)f, (char)g, (char)h, (char)i, (char)j, (char)k, (char)l, (char)m, (char)n, (char)o, (char)p, (char)q, (char)s, (char)t}
-#define b20(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, s, t, u) {(char)a, (char)b, (char)c, (char)d, (char)e, (char)f, (char)g, (char)h, (char)i, (char)j, (char)k, (char)l, (char)m, (char)n, (char)o, (char)p, (char)q, (char)s, (char)t, (char)u}
-
-
-
-//constexpr std::string Convert(const std::array<int, N>& utf8)
-template<size_t N>
-constexpr std::string Convert(int (&arr)[N])
-{
-	std::string ret;
-	for (int i = 0; i < N; ++i)
-		ret.push_back((char)arr[i]);
-	return ret;
-}
-
 #include "ftts.h"
 #include "Processor.h"
 #include "JSProcessor.h"
